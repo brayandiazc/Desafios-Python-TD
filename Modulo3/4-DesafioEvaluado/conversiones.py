7b132d340da878dcc4eaeee451d4401427e514df
Brayan Diaz C <brayandiazc@gmail.com>
2024-02-22 18:11:44 -0500

    [FEAT] Modulo3 4-DesafioEvaluado

---
+++ Modulo3/4-DesafioEvaluado/conversiones.py
@@ -0,0 +1,24 @@
+# Importa la libreria solicitada para recibir argumentos.
+import ...
+
+
+# Variables a utilizar OPCION A.
+tasas = {
+    '...': ...,
+    'clp_parg': ...,
+    '...': float(sys.argv[3])
+}
+
+... = ...(sys.argv[4])
+
+# Variables a utilizar OPCION B.
+... = ...
+clp_parg = ...
+... = float(sys.argv[3])
+
+... = ...(sys.argv[4])
+
+# Mostar el resultado de la conversión.
+print(f"Los {dinero} pesos equivalen a:")
+print(f"...")
+print(f"{tasas[...] * dinero} Pesos Argentinos")
