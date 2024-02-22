5fe89b8d6394c223429cb2810154578bd0864754
Brayan Diaz C <brayandiazc@gmail.com>
2024-02-22 18:27:35 -0500

    [FEAT] Modulo3 4-DesafioEvaluado

---
+++ Modulo3/4-DesafioEvaluado/recordatorios.py
@@ -0,0 +1,8 @@
+recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
+ ['2021-05-01', "15:00", "No trabajar"],
+ ['2021-07-15', "13:00", "No hacer nada es feriado"],
+ ['2021-09-18', "16:00", "Ramadas"],
+ ['2021-12-25', "00:00", "Navidad"]]
+
+# Output
+print(recordatorios)
