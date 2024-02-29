06ec707c928b2455cc1a356cc4e1889ef42d145b
Brayan Diaz C <brayandiazc@gmail.com>
2024-02-29 16:17:05 -0500

    [FEAT] Modulo 2

---
+++ Modulo-3/Unidad-3/DE-EstructurasDatosFunciones-1/recordatorios.py
@@ -0,0 +1,24 @@
+# Viene del material de apoyo.
+recordatorios = [
+	  ['2021-01-01', "11:00", "Levantarse y ejercitar"],
+	  ['2021-05-01', "15:00", "No trabajar"],
+	  ['2021-07-15', "13:00", "No hacer nada es feriado"],
+	  ['2021-09-18', "16:00", "Ramadas"],
+	  ['2021-12-25', "00:00", "Navidad"]
+]
+
+# Requerimiento 3.1.
+recordatorios.insert(...,['2021-01-02', "06:00", "Empezar el año"])
+
+# Requerimiento 3.2.
+recordatorios[...][0] = "2021-07-16"
+
+# Requerimiento 3.3.
+recordatorios....(2)
+
+# Requerimiento 3.4.
+recordatorios...(...,['...', "...", "..."])
+recordatorios.append(["..."])
+
+# Mostrar los recordatorios.
+...(recordatorios)
